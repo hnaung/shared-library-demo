452e1b222c8080871d9a87bb8dee134dcf15c7e2
hnaung <unixaung@gmail.com>
2020-10-05 01:58:50 +0800

    add shared library #3

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,11 +3,15 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    
     node {
 	    // Clean workspace before doing anything
 	    deleteDir()
 
 	    try {
+	        stage ('Clone') {
+	        	checkout scm
+	        }
 	        stage ('Build') {
 	        	sh "echo 'building ${config.yourteam} ...'"
                 sh "docker image build -t ${config.yourteam}/${config.imagename} ."
