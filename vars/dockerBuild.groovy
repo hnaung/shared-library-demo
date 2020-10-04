e12b00c515c05abdaf6c2bb60decc7b1d207c3c1
hnaung <unixaung@gmail.com>
2020-10-05 02:05:18 +0800

    add shared library #6

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -13,7 +13,7 @@ def call(body) {
 	        	checkout scm
 	        }
 	        stage ('Build') {
-	        	sh "echo 'building ${config.projectName} ...'"
+                dockerImage = docker.build ${config.yourTeam}/${config.imageName} + ":$BUILD_NUMBER"
 	        }
 	        stage ('Tests') {
 		        parallel 'static': {
