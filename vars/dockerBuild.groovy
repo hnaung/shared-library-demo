7099fdc99392a0b489f1a4aab591db4ceaee3033
hnaung <unixaung@gmail.com>
2020-10-05 02:51:09 +0800

    add new library #9

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -12,6 +12,23 @@ def call(Map config) {
                 def buildId = "${config.imageVersion}"
 				sh 'docker build -t "${config.imageName}:${buildId}" image/'
 			}
+	        stage ('Tests') {
+		        parallel 'static': {
+		            sh "echo 'shell scripts to run static tests...'"
+		        },
+		        'unit': {
+		            sh "echo 'shell scripts to run unit tests...'"
+		        },
+		        'integration': {
+		            sh "echo 'shell scripts to run integration tests...'"
+		        }
+	        }
+	      	stage ('Deploy') {
+	            sh "echo 'deploying to server ${config.serverDomain}...'"
+	      	}
+	    } catch (err) {
+	        currentBuild.result = 'FAILED'
+	        throw err
 	    }
     }
 }
