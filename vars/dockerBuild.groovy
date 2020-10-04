eb6b926d78c67ea789c941a67d427011590f4a92
hnaung <unixaung@gmail.com>
2020-10-05 02:00:57 +0800

    add shared library #4

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -13,20 +13,25 @@ def call(body) {
 	        	checkout scm
 	        }
 	        stage ('Build') {
-	        	sh "echo 'building ${config.yourteam} ...'"
-                sh "docker image build -t ${config.yourteam}/${config.imagename} ."
-                sh "docker tag ${config.yourteam}/${imagename} ${config.yourteam}/${config.imagename}:${config.ImageTag}"
-                sh "docker tag ${config.yourteam}/${config.imagename} ${config.yourteam}/${config.imagename}:latest"
-                withCredentials([usernamePassword(
-                      credentialsId: "docker",
-                      usernameVariable: "USER",
-                      passwordVariable: "PASS"
-                )]) {
-                    sh "docker login -u '$USER' -p '$PASS'"
-               }
-               sh "docker image push ${config.yourteam}/${config.imagename}:${config.ImageTag}"
-               sh "docker image push ${config.yourteam}/${config.imagename}:latest"
-            }    
-        }
+	        	sh "echo 'building ${config.projectName} ...'"
+	        }
+	        stage ('Tests') {
+		        parallel 'static': {
+		            sh "echo 'shell scripts to run static tests...'"
+		        },
+		        'unit': {
+		            sh "echo 'shell scripts to run unit tests...'"
+		        },
+		        'integration': {
+		            sh "echo 'shell scripts to run integration tests...'"
+		        }
+	        }
+	      	stage ('Deploy') {
+	            sh "echo 'deploying to server ${config.serverDomain}...'"
+	      	}
+	    } catch (err) {
+	        currentBuild.result = 'FAILED'
+	        throw err
+	    }
     }
 }
